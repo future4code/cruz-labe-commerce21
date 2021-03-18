b0a9d1ef20336245295212d9040220f1cdf4d0d4
marcosmaiaideia <marcosmaia@gmail.com>
2021-03-18 13:25:08 -0300

    filtro adicionado e produtos ok

---
+++ src/Produto.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import './Produto.css'
+
+class Produto extends React.Component {
+  state = {
+
+  }
+
+  onClickAdicionarAoCarrinho = () => {
+    this.setState({
+      noCarrinho: !false,
+      numeroCarrinho: this.state.numeroCarrinho + 1,
+      precoCarrinho: this.precoCarrinho + this.props.produtoPreco,
+    })
+  }
+
+  render() {
+
+    return <div className='produto-container'>
+              <div className='produto-foto'>
+                  <img className={'foto'} src={this.props.produtoFoto} alt={'Imagem do produto'}/>
+              </div>
+              
+              <div className='produto-dados'>
+                
+                <p>{this.props.produtoNome}</p>
+                <p>{this.props.produtoPreco}</p>
+              </div>
+              
+              <div className='produto-botao'>
+                  <button onClick={this.props.funcaoBotao}> {this.props.textoBotao} </button>
+              </div>
+            </div>
+  }
+
+}
+
+export default Produto 
